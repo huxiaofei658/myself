21ff76d4b797d8d057b9450fd04ee736cacde78a
hu <hu@LAPTOP-2IDGUO33.lan>
2020-06-28 09:47:33 +0800

    test155

---
+++ src/testgit/test13.java
@@ -0,0 +1,11 @@
+package testgit;
+
+public class test13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		System.out.println("hello" + i);
+	}
+
+}
