f6074191f9e85e3a9f5a9d04b035162c5bcfe8da
hu <hu@LAPTOP-2IDGUO33.lan>
2020-07-23 20:59:56 +0800

    第二次提交

--- src/testgit/test13.java
+++ src/testgit/test13.java
@@ -1,11 +1,33 @@
 package testgit;
 
+import java.util.Random;
+
 public class test13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 0;
-		System.out.println("hello" + i);
+		int max = 0;
+		int min = 0;
+		int num = 0;
+		for (int i = 0; i <= 100; i++) {
+			Random rand = new Random();
+			int r = rand.nextInt(99);
+			System.out.println(r);
+			if (r >= max) {
+				max = r;
+			} else if (r < min) {
+				min = r;
+			}
+			if (r > 50) {
+				++num;
+			}
+
+		}
+
+		System.out.println("最大数max=" + max + "\n" + "最小数min=" + min);
+
+		System.out.println("大于50的个数：" + num);
+
 	}
 
 }
